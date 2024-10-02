3c36e94162cccaf27aaf0ff6829e7fead56967e6
Ihor Bokov <ibokov@playtika.com>
2024-10-03 00:18:26 +0300

    Added docs for MultiFluxionListener and MultiFluxionProvider

--- lib/src/multi_fluxion_listener.dart
+++ lib/src/multi_fluxion_listener.dart
@@ -3,10 +3,51 @@ import 'package:flutter_fluxion/src/base/base_multi_builder.dart';
 import 'package:flutter_fluxion/src/fluxion.dart';
 import 'package:flutter_fluxion/src/fluxion_listener.dart';
 
+/// Signature for builders used in [MultiFluxionListener].
+///
+/// A function that takes a [Widget] child and returns a [FluxionListener]
+/// that wraps the child.
 typedef FlxnListenerBuilder<F extends Fluxion<S>, S extends Object?>
     = FluxionListener<F, S> Function(Widget child);
 
+/// Merges multiple [FluxionListener] widgets into one widget tree.
+///
+/// [MultiFluxionListener] improves the readability and eliminates the need
+/// to nest multiple [FluxionListener]s. By using [MultiFluxionListener],
+/// you can specify multiple [FluxionListener]s in a declarative way.
+///
+/// ```dart
+/// MultiFluxionListener(
+///   listeners: [
+///     (child) => FluxionListener<FluxionA, StateA>(
+///       fluxion: fluxionA,
+///       listener: (context, state) {
+///         // Respond to fluxionA's state changes.
+///       },
+///       child: child,
+///     ),
+///     (child) => FluxionListener<FluxionB, StateB>(
+///       fluxion: fluxionB,
+///       listener: (context, state) {
+///         // Respond to fluxionB's state changes.
+///       },
+///       child: child,
+///     ),
+///   ],
+///   child: MyWidget(),
+/// )
+/// ```
+///
+/// See also:
+///  * [FluxionListener], which listens to a single [Fluxion] instance.
 class MultiFluxionListener extends BaseMultiBuilder {
+  /// Creates a [MultiFluxionListener] widget that merges multiple
+  /// [FluxionListener] widgets into one.
+  ///
+  /// The [listeners] argument is the list of [FlxnListenerBuilder] functions,
+  /// each of which returns a [FluxionListener].
+  ///
+  /// The [child] argument is the widget below this widget in the tree.
   const MultiFluxionListener({
     required List<FlxnListenerBuilder> listeners,
     required super.child,
--- lib/src/multi_fluxion_provider.dart
+++ lib/src/multi_fluxion_provider.dart
@@ -3,10 +3,46 @@ import 'package:flutter_fluxion/src/base/base_multi_builder.dart';
 import 'package:flutter_fluxion/src/fluxion.dart';
 import 'package:flutter_fluxion/src/fluxion_provider.dart';
 
+/// Signature for builders used in [MultiFluxionProvider].
+///
+/// A function that takes a [Widget] child and returns a [FluxionProvider]
+/// that wraps the child.
 typedef FlxnProviderBuilder<F extends Fluxion<S>, S extends Object?>
     = FluxionProvider<F, S> Function(Widget child);
 
+/// Merges multiple [FluxionProvider] widgets into one widget tree.
+///
+/// [MultiFluxionProvider] improves readability and eliminates the need
+/// to nest multiple [FluxionProvider]s. By using [MultiFluxionProvider],
+/// you can provide multiple [Fluxion] instances in a declarative way.
+///
+/// ```dart
+/// MultiFluxionProvider(
+///   providers: [
+///     (child) => FluxionProvider<MyFluxionA, MyStateA>(
+///       create: (context) => MyFluxionA(),
+///       child: child,
+///     ),
+///     (child) => FluxionProvider<MyFluxionB, MyStateB>(
+///       create: (context) => MyFluxionB(),
+///       child: child,
+///     ),
+///   ],
+///   child: MyApp(),
+/// )
+/// ```
+///
+/// See also:
+///  * [FluxionProvider], which provides a single [Fluxion] instance to
+///  the widget tree.
 class MultiFluxionProvider extends BaseMultiBuilder {
+  /// Creates a [MultiFluxionProvider] widget that merges multiple
+  /// [FluxionProvider] widgets into one.
+  ///
+  /// The [providers] argument is the list of [FlxnProviderBuilder] functions,
+  /// each of which returns a [FluxionProvider].
+  ///
+  /// The [child] argument is the widget below this widget in the tree.
   const MultiFluxionProvider({
     required List<FlxnProviderBuilder> providers,
     required super.child,
